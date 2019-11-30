abb366434877f3ed38f69b9f24662e34f53632eb
Andrey Maslov <andreymaslov94@gmail.com>
2019-12-01 02:52:25 +0500

    fully covered UserController with tests

--- src/test/kotlin/ru/aqrc/project/api/web/RestAssuredFacade.kt
+++ src/test/kotlin/ru/aqrc/project/api/web/RestAssuredFacade.kt
@@ -4,6 +4,7 @@ import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import io.restassured.path.json.JsonPath
 import io.restassured.response.ValidatableResponse
 import ru.aqrc.project.api.web.dto.AccountDTO
 import ru.aqrc.project.api.web.dto.UserDTO
@@ -12,7 +13,7 @@ object RestAssuredFacade {
 
     fun getUser(
         userId: String,
-        expectedStatusCode: Int,
+        expectedStatusCode: Int = 200,
         assert: ValidatableResponse.() -> Unit = {}
     ): UserDTO? {
         return get("/users/$userId", expectedStatusCode, assert)
@@ -25,12 +26,32 @@ object RestAssuredFacade {
 
     fun postUser(
         userBody: UserDTO,
-        expectedStatusCode: Int,
+        expectedStatusCode: Int = 200,
         assert: ValidatableResponse.() -> Unit = {}
     ): UserDTO {
         return post("/users", userBody, expectedStatusCode, assert) Extract { `as`(UserDTO::class.java) }
     }
 
+    fun postUserAccount(
+        userId: String,
+        expectedStatusCode: Int = 200,
+        assert: ValidatableResponse.() -> Unit = {}
+    ): AccountDTO {
+        return post("/users/$userId/account", "", expectedStatusCode, assert) Extract { `as`(AccountDTO::class.java) }
+    }
+
+    fun getUserAccounts(
+        userId: String,
+        expectedStatusCode: Int = 200,
+        assert: ValidatableResponse.() -> Unit = {}
+    ): List<AccountDTO>? {
+        return get("/users/$userId/accounts", expectedStatusCode, assert)
+            .let {
+                if (expectedStatusCode == 200)
+                    it.Extract { JsonPath.from(response().asInputStream()).getList<AccountDTO>("accounts") }
+                else null
+            }
+    }
 
     private fun get(
         path: String,
--- src/test/kotlin/ru/aqrc/project/api/web/controller/UserControllerTest.kt
+++ src/test/kotlin/ru/aqrc/project/api/web/controller/UserControllerTest.kt
@@ -2,14 +2,25 @@ package ru.aqrc.project.api.web.controller
 
 import io.javalin.Javalin
 import io.restassured.RestAssured
+import io.restassured.config.JsonConfig.jsonConfig
+import io.restassured.config.RestAssuredConfig
+import io.restassured.path.json.config.JsonPathConfig
 import org.hamcrest.CoreMatchers.*
-import org.junit.jupiter.api.*
+import org.hamcrest.collection.IsCollectionWithSize.hasSize
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.koin.core.Koin
 import ru.aqrc.project.api.config.AppConfig
 import ru.aqrc.project.api.config.KoinConfig
 import ru.aqrc.project.api.web.RestAssuredFacade.getUser
+import ru.aqrc.project.api.web.RestAssuredFacade.getUserAccounts
 import ru.aqrc.project.api.web.RestAssuredFacade.postUser
+import ru.aqrc.project.api.web.RestAssuredFacade.postUserAccount
+import ru.aqrc.project.api.web.dto.AccountDTO
 import ru.aqrc.project.api.web.dto.UserDTO
+import java.math.BigDecimal
 import java.util.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -21,6 +32,9 @@ class UserControllerTest {
         const val ID_PATH = "id"
         const val USERNAME_PATH = "name"
         const val USERNAME = "John Doe"
+        const val USER_ID_PATH = "userId"
+        const val AMOUNT_PATH = "amount"
+        const val ACCOUNTS_PATH = "accounts"
     }
 
     @BeforeAll
@@ -29,6 +43,12 @@ class UserControllerTest {
         api = AppConfig.startApplication()
 
         RestAssured.port = api.port()
+        RestAssured.config = RestAssuredConfig
+            .config()
+            .jsonConfig(
+                jsonConfig()
+                    .numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL)
+            )
     }
 
     @AfterAll
@@ -39,7 +59,7 @@ class UserControllerTest {
 
     @Test
     fun `should create user and return its id`() {
-        postUser(UserDTO(name = USERNAME), 200) {
+        postUser(UserDTO(name = USERNAME)) {
             body(ID_PATH, notNullValue())
             body(USERNAME_PATH, equalTo(USERNAME))
         }
@@ -47,18 +67,59 @@ class UserControllerTest {
 
     @Test
     fun `should get user after creation by its id`() {
-        val user = postUser(UserDTO(name = USERNAME), 200)
+        val user = postUser(UserDTO(name = USERNAME))
 
-        getUser(user.id.toString(), 200) {
+        getUser(user.id.toString()) {
             body(ID_PATH, equalTo(user.id.toString()))
             body(USERNAME_PATH, equalTo(USERNAME))
         }
     }
 
     @Test
-    fun `should respond with 404 and message when user not found`() {
+    fun `should respond with 404 and message on get user when user not found`() {
         val unknownUserId = UUID.randomUUID().toString()
         getUser(unknownUserId, 404) {
+            body("error.message", containsStringIgnoringCase(unknownUserId))
+        }
+    }
+
+    @Test
+    fun `should create account for user with zeroed amount and return it`() {
+        val user = postUser(UserDTO(name = USERNAME))
+
+        postUserAccount(user.id.toString()) {
+            body(ID_PATH, notNullValue())
+            body(USER_ID_PATH, equalTo(user.id.toString()))
+            body(AMOUNT_PATH, equalTo(BigDecimal.valueOf(0, 8)))
+        }
+    }
+
+    @Test
+    fun `should get empty list of user accounts when they were not created`() {
+        val user = postUser(UserDTO(name = USERNAME))
+
+        getUserAccounts(user.id.toString()) {
+            body(ACCOUNTS_PATH, equalTo(emptyList<AccountDTO>()))
+        }
+    }
+
+    @Test
+    fun `should get list of user accounts when they were created`() {
+        val user = postUser(UserDTO(name = USERNAME))
+
+        val account1 = postUserAccount(user.id.toString())
+        val account2 = postUserAccount(user.id.toString())
+
+        getUserAccounts(user.id.toString()) {
+            body("$ACCOUNTS_PATH.$ID_PATH", hasItems(account1.id.toString(), account2.id.toString()))
+            body("$ACCOUNTS_PATH.$ID_PATH", hasSize<List<*>>(2))
+        }
+    }
+
+    @Test
+    fun `should respond with 404 and message on get user accounts when user not found`() {
+        val unknownUserId = UUID.randomUUID().toString()
+        getUserAccounts(unknownUserId, 404) {
             body("error.message", containsStringIgnoringCase(unknownUserId))
         }
     }
